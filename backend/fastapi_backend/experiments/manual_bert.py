e15d9e7afc112fe6b24b8f2eb294037052c0af11
Syed8855 <iamsyedhasnain04@gmail.com>
2026-07-08 21:00:30 +0530

    Implement manual BERT tokenization

---
+++ backend/fastapi_backend/experiments/manual_bert.py
@@ -0,0 +1,17 @@
+from transformers import AutoTokenizer
+from transformers import AutoModelForMaskedLM
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+model = AutoModelForMaskedLM.from_pretrained(
+    "bert-base-uncased"
+)
+
+text = "Machine learning is [MASK]."
+
+inputs = tokenizer(
+    text,
+    return_tensors ="pt"
+)
+print(inputs)
