831998e3c9a7f52e49f563f84f32599feac14369
Syed8855 <iamsyedhasnain04@gmail.com>
2026-07-08 21:33:12 +0530

    Implement manual BERT prediction from logits

--- backend/fastapi_backend/experiments/manual_bert.py
+++ backend/fastapi_backend/experiments/manual_bert.py
@@ -15,5 +15,19 @@ inputs = tokenizer(
     return_tensors ="pt"
 )
 print(inputs)
+
 outputs = model(**inputs)
-print(outputs.logits.shape)
+
+mask_token_index = torch.where(inputs["input_ids"] == tokenizer.mask_token_id)[1]
+
+mask_logits = outputs.logits[
+    0,mask_token_index, :]
+
+probabilities = torch.softmax(mask_logits,dim=-1)
+
+predicted_token_id = torch.argmax(probabilities,dim=-1)
+
+predicted_word = tokenizer.decode(predicted_token_id)
+
+print(predicted_word)
+
